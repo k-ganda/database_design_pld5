ad25136a165b1ed9697aeb7bd964d62fb4e0ca0f
k-ganda <k.ganda@alustudent.com>
2024-11-12 13:09:25 +0300

    new server deployment

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ def delete_user(user_id: int):
     return {"message": f"User with ID {user_id} and all related data have been deleted"}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="localhost", port=8000, reload=True)
